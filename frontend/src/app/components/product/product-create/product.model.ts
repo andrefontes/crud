1758c9ea904fdffcfa063e0422c357fd4d92cf80
André Fontes <andrefontes@gmail.com>
2023-10-24 18:25:20 -0300

    alteração comentario

--- frontend/src/app/components/product/product-create/product.model.ts
+++ frontend/src/app/components/product/product-create/product.model.ts
@@ -1,5 +1,5 @@
 export interface Product {
- id?: number //id é opcional ao criar, o backend que cria.. só precisarar pra alterar e excluir
+ id?: number //id é opcional ao criar, o backend que cria.. só precisara pra alterar e excluir
  name : string
  price: number   
 }
